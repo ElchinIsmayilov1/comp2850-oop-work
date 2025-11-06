4fe11bc9adf6b576c5bc907d0a8cbd6eda412fcf
ElchinIsmayilov1 <148249770+ElchinIsmayilov1@users.noreply.github.com>
2025-11-06 15:38:43 +0000

    Update WordleTest.kt

--- portfolio/wordle/src/test/kotlin/WordleTest.kt
+++ portfolio/wordle/src/test/kotlin/WordleTest.kt
@@ -4,5 +4,31 @@ import io.kotest.matchers.shouldBe
 
 @Suppress("unused")
 class WordleTest : StringSpec({
-    // Write your tests here
+    "should return true for string length 5" {
+        isValid("hello") shouldBe true
+    }
+
+    "should return false for string length !5" {
+        isValid("helo") shouldBe false
+    }
+
+    "should return false for empty string" {
+        isValid("") shouldBe false
+    }
+
+    "should return false for string length 5 but with empty spaces" {
+        isValid("hel o") shouldBe false
+    }
+
+    "should return 2 for finding the word" {
+        evaluateGuess("hello", "HELLO") shouldBe listOf(2, 2, 2, 2, 2)
+    }
+
+    "should return 1 for finding the letter but wrong position" {
+        evaluateGuess("eholl", "HELLO") shouldBe listOf(1, 1, 1, 2, 1)
+    }
+
+    "should return 0 for not finding the letter" {
+        evaluateGuess("guess", "HELLO") shouldBe listOf(0, 0, 1, 0, 0)
+    }
 })
