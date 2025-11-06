ee17df96b2775e1511c8ef6cd8ccd2ba5d73d3d6
ElchinIsmayilov1 <148249770+ElchinIsmayilov1@users.noreply.github.com>
2025-11-06 15:37:07 +0000

    Update Main.kt

--- portfolio/wordle/src/main/kotlin/Main.kt
+++ portfolio/wordle/src/main/kotlin/Main.kt
@@ -1,2 +1,23 @@
 fun main() {
+  val s = readWordList("data/words.txt")
+  val r = pickRandomWord(s)
+  var attempt = 0
+  val correct = listOf(2, 2, 2, 2, 2)
+  while (attempt < 6) {
+      attempt += 1
+      val guess = obtainGuess(attempt)
+      val bar = evaluateGuess(guess, r)
+      if (bar == correct) {
+          println("You Won!")
+          break
+      } else {
+          if (attempt == 6) {
+              println("You lost!")
+              println("Word was $r")
+          } else {
+              displayGuess(guess, bar)
+              println("\nTry Again!")
+          }
+      }
+  }
 }
